87cd50022e060b1eef060df2afa94e3c7a796a20
hardikk69 <h.kansara106@gmail.com>
2026-05-15 16:22:01 +0530

    Fixed Bugs

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { FadeSection, SectionLabel } from '../utils'
 import SuccessModal from '../components/SuccessModal'
 
