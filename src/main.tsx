a8e82827eaf1306b3e3d520c475a0690e9591b02
hardikk69 <h.kansara106@gmail.com>
2026-08-22 16:49:35 +0530

    Fixed GEO

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,28 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { hydrateRoot, createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root')!
+
+// If the root has prerendered content, hydrate it to preserve the HTML.
+// Otherwise fall back to createRoot for dev or non-prerendered scenarios.
+if (rootElement.childNodes.length > 0) {
+  hydrateRoot(
+    rootElement,
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>,
+  )
+} else {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>,
+  )
+}
